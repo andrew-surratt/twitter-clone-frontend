5863c67e541507571f6336b9656586b3ae78dfef
Andrew Surratt <5783385+andrew-surratt@users.noreply.github.com>
2023-07-19 17:46:53 -0400

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios';
 // import { useSession } from "next-auth/client";
 // import { useQuery, useMutation } from "@apollo/client";
 // import { GET_TWEETS, ADD_TWEET } from "../graphql/queries";
@@ -88,6 +89,52 @@ const NewTweet = () => {
   );
 };
 
+const SignIn = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const backendBaseUrl = 'http://localhost:8080';
+        const backendLoginPath = '/login';
+        const data = new URLSearchParams();
+        data.append('username', username);
+        data.append('password', password);
+        axios.post(backendBaseUrl + backendLoginPath, data, {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(console.log)
+            .catch(console.error);
+    };
+
+    return (
+        <div className="content-center">
+            <form onSubmit={handleSubmit} className="flex p-4 border-b border-gray-200">
+            <input
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                placeholder="Username"
+                className="flex-1 ml-4 p-2 border rounded-full outline-none"
+            />
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                className="flex-1 ml-4 p-2 border rounded-full outline-none"
+            />
+            <button
+                type="submit"
+                disabled={!(username || password)}
+                className="bg-blue-500 text-white px-4 py-2 rounded-full ml-4"
+            >Submit</button>
+            </form>
+        </div>
+    );
+}
+
 // Define the main component for rendering the app
 const App = () => {
   const [session] = useSession();
@@ -99,10 +146,16 @@ const App = () => {
   return (
       <div className="max-w-xl mx-auto">
         <h1 className="text-3xl font-bold p-4">Twitter Clone</h1>
-        {session && <NewTweet />}
-        {data.tweets.map((tweet) => (
-            <Tweet key={tweet.id} tweet={tweet} />
-        ))}
+        {session ? (
+            <>
+                <NewTweet />
+                {data.tweets.map((tweet) => (
+                    <Tweet key={tweet.id} tweet={tweet} />
+                ))}
+            </>) : <>
+            <SignIn/>
+        </>
+        }
       </div>
   );
 };
